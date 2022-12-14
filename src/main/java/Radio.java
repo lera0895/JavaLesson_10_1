3331c3ea30242f6fb3ac713eb566ef87b48e1be8
Лера <Lera290897@gmail.com>
2022-12-14 14:19:46 +0300

    Радиоман изменения 14.12.2022

--- src/main/java/Radio.java
+++ src/main/java/Radio.java
@@ -1,74 +1,75 @@
 public class Radio {
 
-    public int currentVolume;
-    public int currentRadioStationNumber;
-    protected  int maxStation;
+
+    private final int soundMaxLayer = 100;
+    private final int soundMinLayer = 0;
+    private final int minStation = 0;
+    protected  int maxStation = 10;
+
 
     public Radio (){
-        maxStation = 9;
     }
-    public Radio(int stationCount) {
-        maxStation= stationCount -1;
+    public Radio(int myMaxStation) {
+        maxStation= myMaxStation;
     }
 
+    private int currentVolume = 0;
+    private int currentRadioStationNumber= 0;
+
+
+
+
+    public void setCurrentVolume(int myVolume) {
+        if (myVolume >= soundMinLayer)
+            if (myVolume <= soundMaxLayer -1) ;
+        {
+            currentVolume = myVolume;
+        }
+    }
+
+
+    public void setCurrentRadioStationNumber(int myStation) {
+        if (myStation >= minStation)
+        if (myStation <= maxStation - 1); {
+            currentRadioStationNumber = myStation;
+        }
+    }
+    public void nextStation() {
+        if (currentRadioStationNumber < maxStation -1) {
+            currentRadioStationNumber++;
+        } else {
+            currentRadioStationNumber = minStation;
+        }
+    }
+
+    public void prevStation() {
+        if (currentRadioStationNumber > minStation) {
+            currentRadioStationNumber--;
+        } else {
+            currentRadioStationNumber = maxStation -1;
+        }
+
+    }
+    public void setMoreLoud() {
+        if (currentVolume < soundMaxLayer) {
+            currentVolume++;
+        } else {
+            currentVolume = soundMaxLayer;
+        }
+    }
+    public void setLessLoud() {
+        if (currentVolume > soundMinLayer) {
+            currentVolume--;
+        } else {
+            currentVolume = soundMinLayer;
+        }
+    }
 
     public int getCurrentVolume() {
         return currentVolume;
     }
 
-    public void setCurrentVolume(int currentVolume) {
-        if (currentVolume < 0) {
-            return;
-        }
-        if (currentVolume > 10) {
-            return;
-        }
-        this.currentVolume = currentVolume;
-    }
-
     public int getCurrentRadioStationNumber() {
         return currentRadioStationNumber;
-    }
-
-    public void setCurrentRadioStationNumber(int currentRadioStationNumber) {
-        if (currentRadioStationNumber < 0) {
-            return;
-        }
-        if (currentRadioStationNumber > maxStation) {
-            return;
-        }
-        this.currentRadioStationNumber = currentRadioStationNumber;
-    }
-
-    public void next() {
-        if (currentRadioStationNumber < maxStation) {
-            currentRadioStationNumber++;
-            return;
-        }
-
-        currentRadioStationNumber = 0;
-    }
-
-    public void prev() {
-        if (currentRadioStationNumber > 0) {
-            currentRadioStationNumber--;
-        } else {
-            currentRadioStationNumber = maxStation;
-        }
-
-    }
-    public void setMoreLoud() {
-        if (currentVolume < 10) {
-            currentVolume++;
-        }else {
-            currentVolume = 0;
-        }
-    }
-    public void setLessLoud() {
-        if (currentVolume > 0) {
-            currentVolume--;
-        }else {
-            currentVolume = 10;
-        }
     }
 }
